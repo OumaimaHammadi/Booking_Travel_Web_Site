ee88aa9384e0806a2be3ad05f23ce7de18729f2d
oumaima <hammadiioumaima@gmail.com>
2026-09-19 01:35:02 +0200

    modifications

--- models/auth.model.js
+++ models/auth.model.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose')
-const bcrypt =require('bcrypt')
+const bcrypt =require('bcryptjs')
 // const DB_URL='mongodb://127.0.0.1:27017/booking-travel'
 const DB_URL='mongodb+srv://hammadiioumaima_db_user:vFSKsZyIxlriCEPa@booking-travel-cluster.ucag5ux.mongodb.net/'
 
